4b631d3d642f1fa2f7bf5e6a230d8ebab6cebc48
0918nobita <nobita.0918@gmail.com>
2018-11-18 16:22:20 +0900

    :sparkles: サイン波の音を含むWAVEファイルを生成するサンプルを追加した

---
+++ ex2_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "wave.h"
+
+int main(void) {
+  MONO_PCM pcm;
+  pcm.fs = 44100;  // 標本化周波数 44.1kHz
+  pcm.bits = 16;   // 量子化精度 16 bit
+  pcm.length = pcm.fs * 1;  // 1 秒間
+  pcm.s = calloc(pcm.length, sizeof(double));
+
+  double a = 0.1,  // 振幅
+        f0 = 500.0;  // 周波数
+
+  for (int n = 0; n < pcm.length; n++)
+    pcm.s[n] = a * sin(2.0 * M_PI * f0 * n / pcm.fs);
+
+  wave_write_16bit_mono(&pcm, "ex2_1.wav");
+
+  return 0;
+}
