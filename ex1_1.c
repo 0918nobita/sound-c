b16f0b76515d09547f9730e12db192584dbf35cb
0918nobita <nobita.0918@gmail.com>
2018-11-18 13:29:13 +0900

    :sparkles: a.wav の内容をコピーして、同じ内容の b.wav を生成するようにした

--- ex1_1.c
+++ ex1_1.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // #include <math.h>  // デバッグ用
 #include "ex1_1.h"
 
 int main(void) {
-  MONO_PCM pcm0;
+  MONO_PCM pcm0, pcm1;
   wave_read_16bit_mono(&pcm0, "a.wav");
+  pcm1.fs = pcm0.fs;
+  pcm1.bits = pcm0.bits;
+  pcm1.length = pcm0.length;
+  pcm1.s = calloc(pcm1.length, sizeof(double));
+  memcpy(&pcm1.s, &pcm0.s, sizeof(pcm0.s));
+  wave_write_16bit_mono(&pcm1, "b.wav");
+  free(pcm1.s);
   return 0;
 }
 
