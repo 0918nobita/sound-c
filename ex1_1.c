53857840d73915f5016c257c486df3e66eaeb5b4
0918nobita <nobita.0918@gmail.com>
2018-11-18 14:04:24 +0900

    :art: Improve readability

--- ex1_1.c
+++ ex1_1.c
@@ -5,13 +5,18 @@
 
 int main(void) {
   MONO_PCM pcm0, pcm1;
+
   wave_read_16bit_mono(&pcm0, "a.wav");
+
   pcm1.fs = pcm0.fs;
   pcm1.bits = pcm0.bits;
   pcm1.length = pcm0.length;
   pcm1.s = calloc(pcm1.length, sizeof(double));
-  memcpy(&pcm1.s, &pcm0.s, sizeof(pcm0.s));
+
+  for (int i = 0; i < pcm0.length; i++) pcm1.s[i] = pcm0.s[i];
+
   wave_write_16bit_mono(&pcm1, "b.wav");
+  free(pcm0.s);
   free(pcm1.s);
   return 0;
 }
