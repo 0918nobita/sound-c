0afaf1751dc09208553e9e18a93bc98221222ce7
0918nobita <nobita.0918@gmail.com>
2018-11-18 14:06:00 +0900

    :art: Improve readability

--- ex1_1.c
+++ ex1_1.c
@@ -16,8 +16,10 @@ int main(void) {
   for (int i = 0; i < pcm0.length; i++) pcm1.s[i] = pcm0.s[i];
 
   wave_write_16bit_mono(&pcm1, "b.wav");
+
   free(pcm0.s);
   free(pcm1.s);
+
   return 0;
 }
 
